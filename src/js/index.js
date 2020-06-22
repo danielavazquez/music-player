6d123325654847878dee7b84a107b08cd5aa6f55
Daniela Vazquez <daniela_056@hotmail.com>
2020-06-22 17:08:44 -0600

    Finish prev and next btn progressContainer and audio eventListeners with respective functions.

--- src/js/index.js
+++ src/js/index.js
@@ -15,9 +15,12 @@ const cover = document.getElementById('cover');
 //*Third keep track of song, initially load song details into DOM variables
 //*Fourth add playBtn eventListener to check status of song within music-container div classList 'play'
 //*Fifth create pauseSong and playSong functions in if stmt within playBtn eventListener
+//*Sixth create prevSong and nextSong eventListeners as well as functions that will trigger with click event
+//*Seventh create time/song update eventListener with updateProgress function
+//*Eighth when song ends make it play next one
 
 //Song titles
-const songs = ['weekend', 'path-of-the-fireflies', 'luminoiz-sunlight'];
+const songs = ['weekend', 'path-of-the-fireflies', 'lumi-sunlight'];
 
 //Keep track of song 
 let songIndex = 0;
@@ -50,6 +53,52 @@ function pauseSong() {
   audio.pause();
 }
 
+//Previous song
+//if its the first song and click back button want to go to the last song
+//if song index is less than 0, then set song index to the last song. Songs.length is 3 so subtract 1 from length
+function prevSong() {
+  songIndex--; //take our song index and decrease by 1
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]); //loadSong and pass in songs arr with current song index after pressing prev button
+
+  playSong();
+}
+
+//Next song
+//songs.length so project is scalable
+function nextSong() {
+  songIndex++; //take our song index and increase by 1
+
+  if (songIndex > songs.length - 1) { //if song index is greater than the length of the array 3-1 = 2
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]); //loadSong and pass in songs arr with current song index after pressing prev button
+
+  playSong();
+}
+
+//Update progress bar
+//event parament part of an eventListener, can get duration and current time from source element
+//const { duration, currentTime } = e.srcElement  console.log(duration, currentTime);
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement
+  progressPercent = (currentTime / duration) * 100 //percentage of song that's been played
+  progress.style.width = `${progressPercent}%`
+}
+
+//Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth; //this takes width of progress bar also gives you a number
+  const clickX = e.offsetX; //when you click it gives you a numerical position 45, 120, 210 etc...
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
 
 //Event Listeners
 //when we click on  play button we want to check if song is playing, if it is want to pause, if not want to play it
@@ -62,3 +111,17 @@ playBtn.addEventListener('click', () => {
     playSong();
   }
 });
+
+//Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//Time/song update
+audio.addEventListener('timeupdate', updateProgress)
+
+//Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+//Song ends
+//nextSong function leverage from change song eventListener
+audio.addEventListener('ended', nextSong);
